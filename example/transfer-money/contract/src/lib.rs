021f94337b551b2fd96a1ad0d75512964f9b8e51
DeadmanBob <github@traxel.com>
2022-06-15 12:52:41 -0700

    skeleton for the demo of a contract that transfers money

---
+++ example/transfer-money/contract/src/lib.rs
@@ -0,0 +1,9 @@
+use solana_program::{
+    // account_info::next_account_info,
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
