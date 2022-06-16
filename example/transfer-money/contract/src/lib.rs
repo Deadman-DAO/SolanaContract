454b691389cf31bcf0803644c2fc1508042957ef
DeadmanBob <github@traxel.com>
2022-06-16 15:26:31 -0700

    restructuring the contract code

--- example/transfer-money/contract/src/lib.rs
+++ example/transfer-money/contract/src/lib.rs
@@ -1,9 +1,6 @@
-use solana_program::{
-    // account_info::next_account_info,
-    account_info::AccountInfo,
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    program_error::ProgramError,
-    pubkey::Pubkey,
-};
+//! A program demonstrating the transfer of lamports
+#![deny(missing_docs)]
+#![forbid(unsafe_code)]
+
+mod entrypoint;
+pub mod processor;
